122584bc0df6f91d5164b31a4b17493d4bc7a1dc
David Park <davepark97@berkeley.edu>
2022-10-17 17:38:31 -0700

    Add class name for each block

--- pvp/script.js
+++ pvp/script.js
@@ -3,14 +3,17 @@ const Blocks = (() => {
   let container = document.querySelector('.gameboard');
 
   const createBlocks = function() {
-    for(let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
       let block = document.createElement('div');
       block.style.border = '2px solid black';
+      block.classList.add('gameboard-block');
       container.appendChild(block).setAttribute('block', i);
     }
   }
 
   return {createBlocks};
 })(); 
+
+
 
 Blocks.createBlocks(); 
