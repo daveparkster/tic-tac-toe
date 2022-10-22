955871c5a873c2d3333fc4a99a92034490a470c6
David Park <davepark97@berkeley.edu>
2022-10-21 19:43:50 -0700

    Remove event listener for pve

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const PVP = document.querySelector('.pvp');
-const PVE = document.querySelector('.pve');
 
 PVP.addEventListener("click", () => {
   window.location.href = './pvp/index.html';
 }); 
-
-PVE.addEventListener("click", () => {
-  window.location.href = './pve/index.html';
-});
